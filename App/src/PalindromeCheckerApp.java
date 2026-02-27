ea5f9333134654bac5e4d8a5b76f146de48af044
kuljot-kaur <kaurkuljot23@gmail.com>
2026-02-27 15:28:48 +0530

    Kuljot Added UseCase2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 /**
  * @author Kuljot
- * @version 1.0
+ * @version 2.0
  */
 public class PalindromeCheckerApp {
 
@@ -9,6 +9,20 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully");
+                String word = "madam";
 
-    }
-}
+                String reversed = "";
+
+                for (int i = word.length() - 1; i >= 0; i--) {
+                    reversed = reversed + word.charAt(i);
+                }
+
+        System.out.println("Input text :" + word);
+                if (word.equals(reversed)) {
+                    System.out.println("Is it a Palindrome? : true");
+                } else {
+                    System.out.println("Is it a Palindrome? : false");
+                }
+            }
+        }
+
